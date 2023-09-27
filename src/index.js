51e45afeaffb37545f9003f3a5bf51fe6f7bf2f2
mrpindro <118183486+mrpindro@users.noreply.github.com>
2023-09-27 11:33:59 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 import thunk from 'redux-thunk'
 import reducers from './reducers';
 import './index.css';
@@ -18,12 +18,8 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider 
-      clientId='176979189211-lcur1aa2c3r946tv6ho5mdtirb76h9ik.apps.googleusercontent.com'
-    >
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
